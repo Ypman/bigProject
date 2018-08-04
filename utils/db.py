efe1df486a23f4de9e311e03eb04bcc2b518ceb0
Ypman <fabianbraeuer1@web.de>
2018-08-04 23:22:51 +0200

    UPDATED VARIOUS NICE FILES! VERY NICE FILES

--- utils/db.py
+++ utils/db.py
@@ -11,8 +11,8 @@ Base = declarative_base()
 
 class ISO(Base):
     __tablename__ = 'iso'
-    hakuna = Column(Integer, primary_key=True)
-    matata = Column(Integer, primary_key=True)
+    hakuna = Column(Integer, primary_key=True, nullable=False)
+    matata = Column(Integer, primary_key=True, nullable=False)
     v1 = Column(Integer)
     v2 = Column(Integer)
     v3 = Column(Integer)
@@ -21,14 +21,22 @@ class ISO(Base):
     country = Column(String(50), nullable=False)
     iso2 = Column(String(2))
     iso3 = Column(String(3))
-    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    # timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return 'hakuna={}, matata={}, v1={}, v2={}, v3={}, p={}, src={}, is2={}, is3={}'\
+            .format(self.hakuna, self.matata, self.v1, self.v2, self.v3, self.province, self.src, self.iso2, self.iso3)
 
 
 engine = create_engine('sqlite:///sqlalchemy_test.db')
 
-session = sessionmaker()
-session.configure(bind=engine)
-Base.metadata.create_all(engine)
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+# Base.metadata.create_all(engine)
+user1 = session.query(ISO).filter_by(iso2="DE").first()
+print(user1)
 
 
 def get_color_values(source_id):
